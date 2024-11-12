33efb5629bcdc850845baacce6081cff9424ac03
Joel <164906226+joel-furrer@users.noreply.github.com>
2024-11-12 18:23:55 +0100

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,140 +1,144 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+  imports = [ ./hardware-configuration.nix ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    #systemd-boot.allow-recovery = true;
+    efi.canTouchEfiVariables = true;
+  };
 
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
+  #hardware.opengl.enable = true;
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+    extraPackages = with pkgs; [
+      mesa
+      mesa_drivers
+      libGL
+      libvdpau
+      libvdpau
+      vulkan-tools
+    ];
+  };
+
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+  };
 
   time.timeZone = "Europe/Zurich";
   i18n.defaultLocale = "de_CH.UTF-8";
 
-  services.xserver.enable = true;
-
-  services.displayManager.sddm.enable = false;
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = false;
-  services.xserver.windowManager.i3.enable = true;
-
-  services.xserver.xkb = {
-    layout = "ch";
-    variant = "de_nodeadkeys";
+  services = {
+    xserver = {
+      videoDrivers = [ "intel" ];
+      enable = true;
+      desktopManager.plasma5.enable = true;
+      windowManager = {
+        i3 = {
+	  enable = false;
+	};
+        awesome = {
+          enable = false;
+          luaModules = with pkgs.luaPackages; [
+            luarocks
+            luadbi-mysql
+          ];
+        };
+      };
+      xkb = {
+        layout = "ch";
+        variant = "de_nodeadkeys";
+      };
+    };
+    displayManager = {
+      sddm.enable = true;
+    };
+    printing.enable = true;
+    pipewire = {
+      enable = true;
+      alsa = { enable = true; support32Bit = true; };
+      pulse.enable = true;
+    };
+    openssh.enable = true;
   };
 
-  fonts.packages = with pkgs; [
-    nerdfonts
-  ];
+  fonts.packages = with pkgs; [ nerdfonts ];
 
   console.keyMap = "sg";
 
-  services.printing.enable = true;
-
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+  hardware = {
+    pulseaudio.enable = false;
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
   };
 
-  users.users.joel = {
-    isNormalUser = true;
-    description = "Joel";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kate
+
+  security.rtkit.enable = true;
+
+  users = {
+    users.joel = {
+      isNormalUser = true;
+      description = "Joel";
+      extraGroups = [ "networkmanager" "wheel" ];
+      packages = with pkgs; [ kate ];
+    };
+  };
+
+  programs = {
+    firefox.enable = true;
+    kdeconnect.enable = true;
+    mtr.enable = true;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowBroken = true;
+
+  environment = {
+    systemPackages = with pkgs; [
+      vim lf neofetch vscodium figlet git obsidian kitty alacritty mesa libreoffice-qt hunspell nmap
+      zsh neovim sl pkgs.python3 pipx gcc openssh nerdfonts direnv steam steam-run gparted android-tools
+      wget ninja spotify vlc tree lua keepassxc jsoncpp zed-editor inkscape libglvnd logisim-evolution
+      discord networkmanager rofi tmux starship telegram-desktop kdeconnect glxinfo simplescreenrecorder
+    ];
+    shells = [
+      "/run/current-system/sw/bin/bash"
+      "/run/current-system/sw/bin/sh"
+      "/nix/store/rdd4pnr4x9rqc9wgbibhngv217w2xvxl-bash-interactive-5.2p26/bin/bash"
+      "/nix/store/rdd4pnr4x9rqc9wgbibhngv217w2xvxl-bash-interactive-5.2p26/bin/sh"
+      "/run/current-system/sw/bin/zsh"
     ];
   };
-
-  programs.firefox.enable = true;
-  nixpkgs.config.allowUnfree = true;
-
-  environment.systemPackages = with pkgs; [
-    vim
-    lf
-    neofetch
-    vscodium
-    figlet
-    git
-    obsidian
-    kitty
-    alacritty
-    zsh
-    neovim
-    sl
-    pkgs.python3
-    pipx
-    gcc
-    openssh
-    nerdfonts
-    direnv
-    qemu
-    wget
-    ninja
-    spotify
-    barrier
-    vlc
-    tree
-    lua
-    keepassxc
-    jsoncpp
-    discord
-    networkmanager
-    feh
-    i3status
-    picom
-    stow
-    polybar
-    killall
-    rofi
-    xorg.xev
-    brightnessctl
-    alsa-utils
-    tmux
-    starship
-    i3-rounded
-  ];
-
-  environment.shells = [
-    "/run/current-system/sw/bin/bash"
-    "/run/current-system/sw/bin/sh"
-    "/nix/store/rdd4pnr4x9rqc9wgbibhngv217w2xvxl-bash-interactive-5.2p26/bin/bash"
-    "/nix/store/rdd4pnr4x9rqc9wgbibhngv217w2xvxl-bash-interactive-5.2p26/bin/sh"
-    "/run/current-system/sw/bin/zsh"
-  ];
-
-  programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
-  services.openssh.enable = true;
 
   networking.firewall = {
     enable = true;
-    allowedTCPPortRanges = [
-      { from = 1714; to = 1764; }
-    ];
-    allowedUDPPortRanges = [
-      { from = 1714; to = 1764; }
-    ];
+    allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+    allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
   };
 
-  programs.kdeconnect.enable = true;
+  fileSystems."/mnt/truenas" = {
+    device = "//192.168.100.101/Daten"; # „Daten“ ist der SMB-Freigabename
+    fsType = "cifs";
+    options = [ "root" "020207" ];
+  };
 
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-
-  virtualisation.virtualbox.host.enable = true;
-  users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
 
   system.stateVersion = "24.05";
 }
-
